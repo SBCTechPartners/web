4370e2eb4c427e63f2208b324503ac97c56ba53f
Steve Hartzog <shartzog@gmail.com>
2014-09-19 15:58:49 -0400

    prepare for ONE

--- build/js/main.js
+++ build/js/main.js
@@ -11,8 +11,8 @@ window.onload = function() {
       case 'index':
          document.getElementById("_index").classList.add("active");
          break;
-      case 'capabilities':
-         document.getElementById("_capabilities").classList.add("active");
+      case 'services':
+         document.getElementById("_services").classList.add("active");
          break;
       case 'partners':
          document.getElementById("_partners").classList.add("active");
@@ -20,8 +20,8 @@ window.onload = function() {
       case 'careers':
          document.getElementById("_careers").classList.add("active");
          break;
-      case 'vehicles':
-         document.getElementById("_vehicles").classList.add("active");
+      case 'customers':
+         document.getElementById("_customers").classList.add("active");
          break;
    }
    resizeMe();
